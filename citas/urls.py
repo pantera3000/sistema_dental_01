aab9da2a46f0c02e7abef9ca9b9e0915b6886bb2
Juan <consultorio@dental.com>
2025-12-11 01:14:12 -0500

    fix: Agregar app_name='citas' a urls.py para resolver NoReverseMatch

--- citas/urls.py
+++ citas/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'citas'
+
 urlpatterns = [
     path('calendario/', views.calendario_view, name='calendario'),
     path('eventos/', views.lista_eventos_view, name='lista_eventos'),
